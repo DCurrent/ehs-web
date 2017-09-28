8e5e10e0adf0614b6f51f62fc64ef83784145a09
Damon V. Caskey <dc@caskeys.com>
2017-09-28 16:36:12 -0400

    Increment all Fire Safety Report file name years by 1.

--- fire/a_reg_archive.php
+++ fire/a_reg_archive.php
@@ -5,14 +5,14 @@
     
     <form action="/libraries/php/general_redirect_0001.php" method="post" name="form1" id="frm_fire_safety_rpt" target="_blank">
         <select name="select" class="small">
-        	<option value="/docs/pdf/fs_annual_fire_safety_report_2016_01.pdf">2017</option>
-        	<option value="/docs/pdf/fs_annual_fire_safety_report_2015_01.pdf">2016</option>
-        	<option value="/docs/pdf/fs_annual_fire_safety_report_2014_01.pdf">2015</option>
-        	<option value="/docs/pdf/fs_annual_fire_safety_report_2013_10.pdf">2014</option>
-        	<option value="/docs/pdf/fs_annual_fire_safety_report_2012_01.pdf">2013</option>
-        	<option value="/docs/pdf/fs_annual_fire_safety_report_2011_01.pdf">2012</option>
-        	<option value="/docs/pdf/fs_annual_fire_safety_report_2010_01.pdf">2011</option>
-        	<option value="/docs/pdf/fs_annual_fire_safety_report_2009_01.pdf">2010</option> 
+        	<option value="/docs/pdf/fs_annual_fire_safety_report_2017_01.pdf">2017</option>
+        	<option value="/docs/pdf/fs_annual_fire_safety_report_2016_01.pdf">2016</option>
+        	<option value="/docs/pdf/fs_annual_fire_safety_report_2015_01.pdf">2015</option>
+        	<option value="/docs/pdf/fs_annual_fire_safety_report_2014_10.pdf">2014</option>
+        	<option value="/docs/pdf/fs_annual_fire_safety_report_2013_01.pdf">2013</option>
+        	<option value="/docs/pdf/fs_annual_fire_safety_report_2012_01.pdf">2012</option>
+        	<option value="/docs/pdf/fs_annual_fire_safety_report_2011_01.pdf">2011</option>
+        	<option value="/docs/pdf/fs_annual_fire_safety_report_2010_01.pdf">2010</option> 
     	</select>
     
     	<input type="submit" name="Submit" value="Go" class="icon_pdf">    
