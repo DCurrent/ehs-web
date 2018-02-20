b6138cc68c6cee29af7f3fae4a9a151a9e859848
Damon V. Caskey <dc@caskeys.com>
2018-02-20 11:20:19 -0500

    Remove Mendeleev app files (it is now an independent repository at https://github.com/DCurrent/Mendeleev).

--- apps/mendeleev_old/source/data_main.php
+++
@@ -1,8 +0,0 @@
-<?php		
-	
-	require_once(__DIR__.'/data_account.php');
-	require_once(__DIR__.'/data_area.php');
-	require_once(__DIR__.'/data_log.php');
-	require_once(__DIR__.'/data_role.php');
-	
-?>
