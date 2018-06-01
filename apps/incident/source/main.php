595a3b83c3d9e088703674fcea4bd5ffe0eafa23
Caskey, Damon V <dc@caskeys.com>
2018-06-01 08:33:49 -0400

    Open Incident report for all users.

--- apps/incident/source/main.php
+++ apps/incident/source/main.php
@@ -10,7 +10,9 @@
 		const SUBMIT_TRUE = 1;
 		const SUBMIT_FALSE = FALSE;
 		const INCIDENT_OB_SES_KEY = 'incident_object';
-		const ACCESS_REPORT = 'dwhibb0, lpoor2, trobert, jghamo2, mla263, asof224';
+		//const ACCESS_REPORT = 'dwhibb0, lpoor2, trobert, jghamo2, mla263, asof224';
+		const ACCESS_REPORT = '';
+		
 		
 		const
 			NO 	= 0,
