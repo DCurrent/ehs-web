466a799e15d6e304234cbe3c680e19b53ccdb4e8
Caskey, Damon V <dc@caskeys.com>
2018-08-15 10:58:28 -0400

    Update Laser Safety verbiage.

--- ohs/laser_safety.php
+++ ohs/laser_safety.php
@@ -28,11 +28,11 @@
 					
 					<h1>Laser Safety</h1>
                     <h2>Statement of Purpose</h2>
-                    <p>The university of Kentucky is dedicated to providing a safe and   healthy working environment for all faculty, staff, students and   visitors. This includes the use of lasers and their safet use.  If you   are interested in learning more about the University of Kentucky&rsquo;s Laser   Safety Program, or have further concerns, please contact the laser   safety officer (LSO), Robert Thomas at 859-257-4016.</p>
-                    <p>UK adopts ANSI Z136.1-2000 as its laser safety program. Exceptions to   this standard will be considered on a case-by-case basis by the LSO.   The LSO shall document and keep record of any policy decisions that are   exceptions to the ANSI Z136.1- 2000 standard.</p>
+                    <p>The University of Kentucky is dedicated   to providing a safe and healthy working environment for all faculty,   staff, students and visitors. This includes the use of lasers.  If you   are interested in learning more about the University of Kentucky&rsquo;s Laser   Safety Program, or have further concerns, please contact the laser   safety officer (LSO), Robert Thomas at 859-257-4016.</p>
+                    <p>UK adopts ANSI Z136.1-2000 as its laser   safety program. Exceptions to this standard will be considered on a   case-by-case basis by the LSO. The LSO shall document and keep record of   any policy decisions that are exceptions to the ANSI Z136.1- 2000   standard.</p>
                     <p class="alert">Attention: All class 3b and 4 lasers and laser systems and users MUST be registered!</p>
-                  <p>RC&amp;RS Laser Safety maintains records for all class 3b and 4   lasers and laser systems used in university facilities and is currently   updating the registration database.</p>
-                    <p>All new and existing class 3b and 4 lasers and laser systems must be   registered. This registration also includes lasers or laser systems that   are in storage or are out-of-service.</p>
+                  <p>UK OHS Laser Safety maintains records for   all class 3b and 4 lasers and laser systems used in university   facilities and is currently updating the registration database.</p>
+                    <p>All new and existing class 3b and 4   lasers and laser systems must be registered. This registration also   includes lasers or laser systems that are in storage or are   out-of-service.</p>
                     <p><a href="../radiation/laser_fs.php">Fact Sheet</a><br>
                       <a href="https://ehs.uky.edu/docs/pdf/rad_laser_safety_manual.pdf#page=11">Laser Registration</a><br>
                       <a href="https://ehs.uky.edu/docs/pdf/rad_laser_safety_manual.pdf">Laser Safety Manua</a>l </p>
