6b092ca753c90505d3f6c82cc784f0f2d01c2346
Caskey, Damon V <dc@caskeys.com>
2019-07-15 20:15:38 -0400

    Send latest bioraft file.

---
+++ bioraft.php
@@ -0,0 +1,60 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT']."/libraries/php/classes/config.php"); //Basic configuration file. ?>
+
+<!DOCtype html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "//www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="//www.w3.org/1999/xhtml">
+<head>
+<title>UK - Occupational Health &amp; Safety, Setup and Exit Survey Checklist</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.css">
+<link rel="stylesheet" href="libraries/css/style.css" type="text/css" />
+
+<link rel="stylesheet" href="libraries/css/print.css" type="text/css" media="print" />
+
+
+</head>
+
+<body>
+
+<div id="container">
+	<div id="mainNavigation">
+		<?php include($cDocroot."libraries/includes/inc_mainnav.php"); ?>
+	</div>
+	<div id="subContainer">
+		<?php include($cLRoot."a_banner_0001.php"); ?>
+		<div id="subNavigation">
+		 	<?php include($cLRoot."a_subnav_0001.php"); ?> 
+		</div>
+		<div id="content">
+			<h1>BioRaft Audit Application</h1>
+		  <p>The UK Environmental Health, and Safety (EHS) Division is  pleased to announce the implementation of a new laboratory audit inspection  system known as BioRaft. The EHS departments who will be using this system to  perform laboratory audits/inspections are Occupational Health and Safety (OHS),  Environmental Management (EMD), Biological Safety (BS), and Radiation Safety  (RS). This system will provide a single portal for audit checklists, audit  records, communications, and audit history for laboratory spaces. As the system  is implemented, the audit results from the EHS departments will be stored in  the system. Notification of audit results will be communicated through the  system with an email to your Inbox. You will be able to respond to the audit  including attaching photos or other documentation. Additionally, the system has  some information regarding regulatory violations applicable to audit findings. </p>
+          <p>In order to begin using the system, each PI is required to  set up their laboratory in the system utilizing a General Setup Wizard. </p>
+          <p><a href="https://uky.bioraft.com/" target="_blank">The BioRAFT system may be accessed here</a>.  Log in using your LinkBlue ID and password.&nbsp; </p>
+          <p><a href="https://dib.uky.edu/ehs/sites/ehs.uky.edu/files/Bioraft%20PI%20Training.pdf" target="_blank">This PI Help Guide</a> provides instructions for laboratory set  up and additional details about the system. Please review before beginning. </p>
+          <p>If you are a new PI at the University of Kentucky, email <a href="mailto:bioraft@uky.edu">bioraft@uky.edu</a> to be designated as a PI in  UKY BioRaft.</p>
+          <p>For additional assistance for UKY BioRAFT, contact us at: <a href="mailto:bioraft@uky.edu">bioraft@uky.edu</a> or <a href="tel:+18593237345">(859) 323-7345</a>.</p>
+          <p>&nbsp;</p>
+      </div>       
+	</div>    
+	<div id="sidePanel">		
+		<?php include($cLRoot."a_sidepanel_0001.php"); ?>		
+	</div>
+	<div id="footer">
+		<?php include($cDocroot."libraries/includes/inc_footer.php"); ?>		
+	</div>
+</div>
+
+<div id="footerPad">
+<?php include($cDocroot."libraries/includes/inc_footerpad.php"); ?>
+</div>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-40196994-1', 'uky.edu');
+  ga('send', 'pageview');
+
+</script>
+</body>
+</html>
