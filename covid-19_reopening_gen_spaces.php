8e3f01b8ad427382dfb571f20fb6d4a6d6300f13
Caskey, Damon V <dc@caskeys.com>
2020-07-21 09:18:39 -0400

    Add coivid-19 Reopening General Spaces guidelines.

---
+++ covid-19_reopening_gen_spaces.php
@@ -0,0 +1,169 @@
+<?php require($_SERVER['DOCUMENT_ROOT']."/libraries/php/classes/config.php"); //Basic configuration file. ?>
+
+<!DOCtype html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>UK - Environmental Health And Safety</title>        
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.css" />
+		<link rel="stylesheet" href="libraries/css/style.css" type="text/css" />
+        <link rel="stylesheet" href="libraries/css/print.css" type="text/css" media="print" />
+    </head>
+    
+    <body>    
+        <div id="container">            
+            <div id="mainNavigation">
+                <?php include($cDocroot."libraries/includes/inc_mainnav.php"); ?>
+            </div><!--/mainNavigation-->            
+            <div id="subContainer">                            
+				<?php include("a_banner_0001.php"); ?>                               
+                <div id="subNavigation">                
+                    <?php include("a_subnav_0001.php"); ?>                     
+                </div><!--/subNavigation-->                
+              <div id="content">                
+                    <h1>Reopening General Spaces</h1>
+				
+				  <div class="no_break">
+				  <h2>Continuing Telework</h2>
+				  <ul>
+				    <li>Continue to promote telework to the greatest extent feasible.</li>
+			    </ul>
+				</div>
+				
+				<div class="no_break">
+				  <h2>Social Distancing</h2>
+				  
+				  <ul>
+					  <li>Employees who are not able to telework and must be physically present at the office must remain a minimum of six (6) feet away from all other employees and customers unless closer interaction is absolutely required to perform their job duties.</li>
+				  </ul>
+				</div>
+				
+				<div class="no_break">
+				<h2>Mask/Cloth Face Coverings Use</h2>
+					
+					<ul>
+					  <li>Masks/Cloth Face Coverings are worn to avoid spreading the virus that causes COVID-19. Employees coming to campus should have a Mask/Cloth Face Covering with them or they will be provided one. It must always be worn  unless individuals are alone in a room, eating, drinking or exercising or when it interferes with required curricular activities.</li>
+			    </ul>
+				</div>
+				
+				<div class="no_break">
+				<h2>Hand Washing/Hand Sanitizer</h2>
+					
+					<ul>
+					  <li>Encourage frequent handwashing and ensure hand sanitizer is available near high-traffic and high touch areas.  Entry areas of buildings have already been evaluated for sanitizer placement.</li>
+			    </ul>
+				</div>
+				
+				<div class="no_break">
+				<h2>Cleaning of Areas/Surfaces</h2>
+					
+					<ul>
+					  <li>Custodial Services will continue to clean facilities on a routine basis. However, high contact surfaces require disinfection before each use (e.g., doorknobs, keyboards, remote controls, desks, shared keyboards, refrigerator handles, microwave oven buttons, other work tools and equipment) with consumer cleaning products.  Providing disposable disinfecting wipes will allow employees to wipe down commonly used surfaces before each use. These products can be purchased via the UK Supply Center. It will be the building occupant’s responsibility to identify these high touch surfaces and ensure they are disinfected.</li>
+			    </ul>
+				</div>
+				
+				<div class="no_break">
+				<h2>Office Space</h2>
+					
+					<ul>
+					  <li>Prior to re-occupancy, perform a detailed review of the configuration of your workspaces:
+					    <ul>
+					      <li>Consider  eliminating reception seating areas and mandate visitors are only received via  appointment.</li>
+					      <li>Review  floorplans and remove or reconfigure seats, furniture and workstations as  needed to ensure a minimum of 6 feet is maintained between building occupants.</li>
+					      <li>Reconfigure  workstations so that employees do not face each other.</li>
+					      <li>Cubicle  workstations alone do not preclude the requirement for 6 feet separation.</li>
+					      <li>Temporarily  remove amenities that are handled with high contact frequency, such as water  coolers, coffee maker, and bulk snacks.</li>
+				        </ul>
+					  </li>
+					  <li>If  vending machines are used, provide disinfectant wipes to allow wipe down after  each use.</li>
+					  <li>Reduce  tasks requiring large amounts of people to be in one area.</li>
+					  <li>Employees  should use virtual meeting tools in lieu of in-person meetings whenever  possible. </li>
+			    </ul>
+				</div>
+				  
+				<div class="no_break">
+				<h2>Conference  Rooms</h2>
+					<ul>
+					  <li>Conference Rooms that are used should be  disinfected daily at a minimum.</li>
+					  <ul>
+					    <li>Disinfectant  wipe or spray should be in each conference room and employees should wipe down  all surfaces and equipment (e.g., mouse, keyboard, phone) touched during  conference room meetings.</li>
+				      </ul>
+					  <li>Consider limiting in-person meetings  to 10 people or less, if virtual meetings are  not feasible.  Separation of 6 feet  between meeting attendees must be maintained.</li>
+					  <li>In-person meetings should be of the shortest  duration possible. </li>
+			    </ul>
+				</div>	
+				  
+				  <div class="no_break">
+				  <h2>Lobby and Common Areas</h2>
+				  <ul>
+				    <li>When receiving members of the general public,  utilize a sign-in register to record the date, time, visitor name, person seen,  and location visited.</li>
+				    <li>Plexiglas barriers should be considered for  reception desks and other stations where interaction with the public would occur  and a 6-foot separation is not practical. Recommended barrier height range is  66&rdquo; – 72&rdquo; to account for 95% of the population. Contact <a href="mailto:melissa.claar@uky.edu">Melissa Claar</a> for additional  information.</li>
+				    <li>Use signs, tape marks, or other visual cues such  as decals or colored tape on the floor, placed 6 feet apart, to indicate where  to stand when physical barriers are not possible.</li>
+				    <li>The use of common areas should be discontinued  if possible.</li>
+				    <li>Common areas (e.g., lobby, reception areas)  should be cleaned and disinfected daily at a minimum.</li>
+				    <li>Regulate the use of common areas with  conspicuous signage (including maximum occupancy) and physical distancing  measures.</li>
+				    <ul>
+				      <li>Provide consumer cleaning supplies for employees  to utilize before/after they use common spaces and contact surfaces. </li>
+				      <li>Encourage staff not to linger or socialize in  common areas.</li>
+			        </ul>
+				    <li>Disinfect all surfaces and commonly touched equipment after each use (e.g., sign in  devices).</li>
+				    <li>Communal meals should not be provided to  employees and food not made available in common areas where employees my  congregate. </li>
+		        </ul>
+				  </div>
+				
+				  <div class="no_break">
+				  <h2>Kitchens</h2>
+				  <ul>
+				    <li>Kitchen areas should be clean and disinfected  daily at a minimum.</li>
+				    <li>Kitchen equipment should also be cleaned on a  routine basis:</li>
+				    <ul>
+				      <li>Refrigerator handles, microwave handles, and ice  machine handle should be disinfected at least three times per day.  Communal coffee machines should not be  allowed.</li>
+				      <li>The outside of dishwashers should be cleaned at  the beginning of each shift.</li>
+				      <li>Personal utensils should not be kept in kitchen  area – use disposable utensils or keep in lunch sack. </li>
+				      <li>Ice machines requiring the use of a handheld  scoop should not be used.</li>
+			        </ul>
+				    <li>Water/beverage facets that require employees to  operate them with their hands should be disinfected three times per day. </li>
+				    <li>Disposable utensils and dishes are recommended. </li>
+			    </ul>
+				  </div>
+				
+					<div class="no_break">
+				  <h2>Restrooms</h2>
+				  <ul>
+				    <li>Doors to multi-stall restrooms should be able to  be opened without touching handles if possible.</li>
+				    <ul>
+				      <li>Place a trash can by the door if the door cannot  be opened without touching the handle.</li>
+				      <li>Place signs indicating that toilet lids (if present)  should be closed prior to flushing.</li>
+				      <li>Place signs indicting the need to wash hands for  at least 20 seconds.</li>
+				      <li>Provide paper towels and discontinue the use of  hand air dryers.</li>
+				      <li>Restrooms should be cleaned and disinfected on a  regular basis.</li>
+			        </ul>
+			    </ul>
+				</div>
+				  
+				  </div><!--/content-->                      
+            </div><!--/subContainer-->
+            
+            <div id="sidePanel">
+                <?php include($cDocroot."a_sidepanel_0001.php"); ?>		
+            </div><!--/sidePanel-->
+            
+            <div id="footer">
+                <?php include($cDocroot."libraries/includes/inc_footer.php"); ?>		
+            </div><!--/footer-->
+        </div><!--container-->
+        
+        <div id="footerPad">
+        	<?php include($cDocroot."libraries/includes/inc_footerpad.php"); ?>
+        </div><!--/footerPad-->
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-40196994-1', 'uky.edu');
+  ga('send', 'pageview');
+
+</script>
+</body>
+</html>
