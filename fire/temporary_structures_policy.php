e410b6bdbe05ecac471ffd1b48b8a9870cb51957
Caskey, Damon V <dc@caskeys.com>
2020-10-24 13:43:57 -0400

    Update tent policy.

---
+++ fire/temporary_structures_policy.php
@@ -0,0 +1,205 @@
+<?php 
+	abstract class PATH
+	{		
+		const ROOT = '../';
+		const PARENT = '../fire/';
+	}
+
+	require(PATH::ROOT."libraries/php/classes/config.php"); //Basic configuration file.
+?>
+
+<!DOCtype html>
+    <head>
+        <title>UK - Environmental Health And Safety</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.css">
+<link rel="stylesheet" href="../libraries/css/style.css" type="text/css" />
+
+        <link rel="stylesheet" href="../libraries/css/print.css" type="text/css" media="print" />
+    <style>
+		th
+{
+	background-color: 			#224D7A;
+	color: 						#FFF;
+	font-weight: 				bold;
+	font-size:small;
+	text-align:					center;
+	padding:					3px;
+	width:						auto;
+}</style>
+
+	</head>
+    
+	
+
+    <body>    
+        <div id="container">
+            
+            <div id="mainNavigation">
+                <?php include(PATH::ROOT."libraries/includes/inc_mainnav.php"); ?>
+            </div><!--/mainNavigation-->
+            
+            <div id="subContainer">
+                
+				<?php include(PATH::PARENT."a_banner_0001.php"); ?>
+                
+                <div id="subNavigation">
+                    <?php include(PATH::PARENT."a_subnav_0001.php"); ?> 
+                </div><!--/subNavigation-->
+                
+                <div id="content">
+                
+                    <h1>Temporary Structures Policy</h1>
+                    <h2>Objective</h2>
+                      This  policy  sets forth the requirements for  the permitting and approval of any tent or other  temporary structure erected on any property owned by the University of Kentucky (UK). It also provides  standards  for the safe operation  and use of the temporary structure.</p>
+				
+                  <h2>Applicability</h2>
+                      <p>This policy applies to any tent or other temporary structure  erected, operated, or maintained for any purpose on any property owned by  UK. The policy requirements shall  apply to all temporary structures erected on the main campus  for clinical  operations,     athletic events, and for  the establishment of a  satellite campus used for agricultural purposes and owned by the University.</p>
+                  <p> Tents not exceeding 400 square feet used exclusively by athletic  event patrons outside of a stadium or arena shall be excluded from this policy.  Additionally, tents not exceeding 400 square feet that have been submitted and  approved by  UK Event Management are excluded by  this policy. Site placement requirements may still apply in either instance.
+                        </p>
+                      </p>
+			<h2>Definitions</h2>
+                  <p>Temporary  Structure –  any  structure erected for human occupancy on a temporary basis for less than 180  days within a 12-month period. Temporary structures include, but are not  limited to  membrane structures, tents,  canopies, stages, and platforms.</p>
+                    <p>Tents  –  a  structure, enclosure, or shelter, with or without sidewalls, constructed of  pliable material supported in any manner except by air or the contents it  protects.</p>
+			<h2>Background</h2>
+                      <p>Tents and other temporary structures are commonly used for a  variety of reasons on the  UK campus. Each one of these  structures must meet certain requirements for structural stability, weather  resistance, appropriate egress components, lighting, and method of  installation. To ensure that all temporary structures meet the Kentucky Building  Code (KBC) requirements, each one must be submitted for permitting and approval  through the UK Fire Marshal&rsquo;s Office.<br>
+                      The  KBC requires that all temporary  structures erected for human occupancy be submitted to the Authority Having  Jurisdiction (AHJ) for permitting and approval. The UK Fire Marshal&rsquo;s Office is  the AHJ and has provided this policy and the associated documents to assist  users in completing this process.</p> 
+			
+                    <h2>Temporary Structure Policy</h2>
+                      <p>Tents and other temporary structures intended for human  occupancy shall not be erected on any University of Kentucky owned property  before being submitted for permitting, review, and approval by the  UK Fire Marshal&rsquo;s  Office. Any temporary structure found  to be erected without being permitted and approved shall be vacated immediately  and removed at the discretion of the UK Fire  Marshal&rsquo;s Office. If removal of the temporary structure is necessary, the <u>UK </u>Fire Marshal&rsquo;s Office will  notify the responsible department or unit person(s) in writing and the  structure shall be taken down within 48 hours of notification. Monetary fees  associated with removal of the temporary structure will be the responsibility  of the department/unit.</p>
+                    
+			<h2>Temporary Structure Requirements and Approval Process</h2>
+                  <p>All temporary structures as defined previously must be  submitted to and approved by the UK Fire Marshal&rsquo;s Office before placement on  University owned property. The process of approval begins with one of the  following two options:</p>
+                  <ul>
+                      <li>State model approval - The Kentucky Department of Housing, Building, and Construction administers a  model approval program for temporary structures. Using this process to get  certification for the temporary structure means that any future submittals for  the approved model number will not require the resubmission of drawings and/or  a review of the drawings. If a structure is approved by the state and added to  the program, only the manufacturer, model number, and site placement details  will need to be submitted to the UK Fire Marshal&rsquo;s Office  for review and approval.<br>
+                      For further information, please refer to this document: <a href="http://dhbc.ky.gov/Documents/KHBC_%20TENT%20Temporary%20Structure%20Guide%20rev%2011-12-13%20Final.pdf" target="_blank">KY Temporary Structure Guidelines</a></li>
+                      <li>Event and site-specific engineering analysis - If no state model approval exists, tents and other temporary structure  submissions shall include the required construction and site documents along  with an engineering analysis completed and bearing the required signature and  seal of a registered design professional.</li>
+                    </ul>
+                  <p>The application documents required for the University Fire  Marshal are found here:</p>
+                  <ul>
+                    <li><a href="UK_TemporaryStructure_ModelSubmittal_v2_form.pdf" target="_blank">Temporary Structure Model Sumbittal Form</a> – To be  completed by the representative supplying the temporary structure.</li>
+                    <li><a href="UK_TemporaryStructure_SiteSubmittal_v2_form.pdf" target="_blank">Temporary Structure Site Submittal Form</a> – To be  completed by the unit or department requesting the temporary.</li>
+                  </ul>
+Completed forms and documentation can be emailed  to <a href="mailto:brandon.ernest@uky.edu">Brandon Ernest</a>.
+                    
+                    <p>The following requirements will be in effect for all temporary  structures that are approved for placement.<br>
+                      </p>
+                    <h4>Wind Speed Capacity</h4>
+			
+                    <ul>
+                      <li>No tent, regardless of maximum wind speed noted  on the approval, shall be occupied when sustained wind speeds reach 40 mph or  greater. </li>
+                      <li>If the maximum wind speed on the approval is 40  mph or less, the tent shall be vacated when wind speeds reach 25 mph or 75% of  the established wind speed capacity.</li>
+                  </ul>
+                    <h4>Use Period</h4>
+                      Temporary structures shall not be erected for a period of  more than 180 days. Structures that may need an additional amount of time must  be resubmitted at the end of the period.
+  <h4>Location</h4>
+                      Temporary structures shall not be located within 20 feet of  lot lines, buildings, other temporary structures, or parked vehicles without  prior approval from the UK Fire Marshal&rsquo;s Office.<br>
+  <h4>Means of Egress</h4>
+                    <ul>
+                      <li>Exits shall be spaced at approximately equal  intervals around the perimeter of the structure and located such that all  points are 100 feet or less from an exit.</li>
+                      <li>The number of exits provided shall not be less  than the number of exits shown below:<br>
+                        <br>
+                      </li>
+                    
+                      <table>
+                        <tr>
+                          <th>Occupant Load</th>
+                          <th>Minimum Number of Exits</th>
+                          <th>Minimum Width of Each Exit</th>
+                        </tr>
+                        <tr>
+                          <td>10-199</td>
+                          <td>2</td>
+                          <td>72 inches</td>
+                        </tr>
+                        <tr>
+                          <td>200-499</td>
+                          <td>3</td>
+                          <td>72 inches</td>
+                        </tr>
+                        <tr>
+                          <td>500-999</td>
+                          <td>4</td>
+                          <td>96 inches</td>
+                        </tr>
+                        <tr>
+                          <td>1,000-1,999</td>
+                          <td>5</td>
+                          <td>120 inches</td>
+                        </tr>
+                      </table>
+                    
+                    
+                      <li>Exit  openings may be covered with a contrasting colored, flame-resistant  curtain. The curtain must be free-sliding and mounted on a metal support at  least 80 inches above the ground.</li>
+                      <li>Aisles must be provided for in the furniture and  equipment placement. Aisles must be at least 44&rdquo; in width and maintained  throughout the use of the structure. Larger aisles may be required with  increased occupant loads.</li>
+                      <li>Structures that are publicly accessible must  have smooth-surfaced aisles for ADA access, subject to approval  by  the UK Fire Marshal&rsquo;s Office.</li>
+                      <li>Exits  shall be clearly marked. Exit signs shall be installed at required exits and  will require external or internal illumination of the sign when the structure  is occupied. Power to illuminate the exit signs shall be supplied from a  separate circuit or source of power.</li>
+                      <li>Means of egress shall always be illuminated when  the structure is occupied. Fixtures required to provide this illumination shall  be supplied from a separate circuit or source of power.</li>
+                    </ul>
+                    <h4>Certification Notice</h4>
+                      <p>A sign shall be affixed to the tent that states the manufacturer,  model number, UK Fire Marshal&rsquo;s Office approval number, occupancy  number, and maximum wind speed capacity.</p>
+                    <h4>General Requirements</h4>
+                      <p>General requirements for the use and operation of the temporary  structures. It will be the responsibility of the department/unit to ensure that  all requirements listed in this policy are met and maintained. Inspections shall  be conducted frequently by the department/unit and immediately after any severe  weather event. </p>
+                    <ul>
+                      <li>All tents, canopies, and membrane structures  shall be constructed of materials that meet the flame propagation performance  criteria of NFPA 701 or other testing standard approved by the UK Fire  Marshal&rsquo;s Office.
+                      </li>
+                      <li>Electrical installations must be submitted to,  approved, and inspected by the Kentucky Housing, Building, and Construction  Electrical Division. 
+                      </li>
+                      <li>Required egress as noted in the previous section  shall be available and maintained at all times.
+                      </li>
+                      <li>Anchoring ropes and/or guylines shall be flagged  to be easily recognizable. Anchors extending above the ground shall be capped.<br>
+                      </li>
+                      <li>Combustible materials such as hay, straw,  shavings, or similar items shall not be located inside of any temporary  structure. The area inside and surrounding the structure within 20 feet shall  also remain clear of any combustible items unless approved by the UK Fire  Marshal&rsquo;s Office. If these items are approved for use, they must be wetted thoroughly  before occupancy of the structure.<br>
+                      </li>
+                      <li>Smoking shall not be permitted inside of or near  any temporary structure.<br>
+                      </li>
+                      <li>Open flames or any devices emitting flame, fire  or heat, or any flammable or combustible liquids, gas, charcoal, or other  cooking devices shall not be located inside or within 20 feet of the temporary structure.<br>
+                      </li>
+                      <li>Fireworks shall not be used within 100 feet of a  temporary structure and any fireworks must be approved by the UK Fire Marshal&rsquo;s  Office at least 30 days before use.<br>
+                      </li>
+                      <li>Spot lighting shall be protected from  combustible construction located within 6 feet with approved methods.<br>
+                      </li>
+                      <li>There shall be a minimum clearance of 3 feet  between the fabric envelope and all contents located inside the temporary  structure.<br>
+                      </li>
+                      <li>Portable fire extinguishers shall be provided.<br>
+                      </li>
+                      <li>Heating equipment must be approved for use and  placement by the UK Fire Marshal&rsquo;s Office.<br>
+                      </li>
+                      <li>LP gas tanks approved for use must be located at  least 10 feet from the structure. The tank and all appurtenances shall be  protected from access by the general public.<br>
+                      </li>
+                      <li>Generators approved for use must be located at  least 20 feet from the structure. The generator and all wiring shall be  protected from access by the general public.<br>
+                      </li>
+                      <li>Flammable and combustible liquids shall not be  used or stored inside temporary structures. If approved, these materials must  be stored at least 50 feet from the structure.</li>
+                    </ul>
+                    <hr>
+                    <p>Last Update: 2020-08-20 10:45</p>
+                </div>
+                <!--/content-->
+            
+            </div><!--/subContainer-->
+            
+            <div id="sidePanel">		
+                <?php include(PATH::PARENT."a_sidepanel_0001.php"); ?>		
+            </div><!--/sidePanel-->
+            
+            <div id="footer">
+                <?php include(PATH::ROOT."libraries/includes/inc_footer.php"); ?>		
+            </div><!--/footer-->
+            
+        </div><!--/container-->
+        
+        <div id="footerPad">
+        	<?php include(PATH::ROOT."libraries/includes/inc_footerpad.php"); ?>
+        </div><!--/footerPad-->
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-40196994-1', 'uky.edu');
+  ga('send', 'pageview');
+
+</script>
+</body>
+</html>
