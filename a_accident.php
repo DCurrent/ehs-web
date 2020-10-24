8ecf84faed6b1b1c43afe15b48306bf94602d0ad
Caskey, Damon V <dc@caskeys.com>
2020-10-24 13:37:01 -0400

    Update accident report phone number.

--- a_accident.php
+++ a_accident.php
@@ -5,7 +5,7 @@
     <p>Work Related <span class="color_red">Employee</span> Injuries <br />800-440-6285</p>
     <p>Student or <span class="color_red">Visitor</span> Injuries<br /> 
     <a href="/ohs/accident.php">Injury and Illness Reporting</a></p>
-  <p>Report <span class="color_red">unsafe conditions</span> to EHS<br />(859) 257-3827 or <a href="mailto:dwhibb0@email.uky.edu">send us an e-mail</a>.</p>
+  <p>Report <span class="color_red">unsafe conditions</span> to EHS<br />(859) 257-2924 or <a href="mailto:dwhibb0@email.uky.edu">send us an e-mail</a>.</p>
     <p> After hours and weekends<br />UKPD (859) 257-1616 <span class="color_red">Emergencies</span> 911!</p> 
 
     
