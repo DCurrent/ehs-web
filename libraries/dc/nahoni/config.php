cf539cfa82f87f49ab47734970c7e7e4950d11b9
Caskey, Damon V <dc@caskeys.com>
2020-12-24 23:39:46 -0500

    Individiual declaration of Nahoni default constants.

--- libraries/dc/nahoni/config.php
+++ libraries/dc/nahoni/config.php
@@ -7,11 +7,11 @@ abstract class DEFAULTS
 	const 	LIFE		= 1440;					// Default session lifetime (seconds).
 			
 			// Stored procedure default names.
-	const	SP_PREFIX	= NULL,							// Prefix for all other SP names.
-			SP_CLEAN	= 'dc_nahoni_session_clean',	// SP to remove all expired sessions.
-			SP_DESTROY	= 'dc_nahoni_session_destroy',	// SP to remove single session on command.
-			SP_GET		= 'dc_nahoni_session_get',		// SP to read session data.
-			SP_SET		= 'dc_nahoni_session_set';		// SP to write session data.
+	const	SP_PREFIX	= NULL;							// Prefix for all other SP names.
+	const	SP_CLEAN	= 'dc_nahoni_session_clean';	// SP to remove all expired sessions.
+	const	SP_DESTROY	= 'dc_nahoni_session_destroy';	// SP to remove single session on command.
+	const	SP_GET		= 'dc_nahoni_session_get';		// SP to read session data.
+	const	SP_SET		= 'dc_nahoni_session_set';		// SP to write session data.
 }
 
 ?>
