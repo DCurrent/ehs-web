925d1d1fd744c1690b902ef8e33a16a76a0a3b77
Caskey, Damon V <dc@caskeys.com>
2021-01-19 13:01:30 -0500

    Nahoni session data defaults to empty string. PHP 7.1+ throws error on NULL.

--- apps/waukegan/source/dc/nahoni/Data.class.php
+++ apps/waukegan/source/dc/nahoni/Data.class.php
@@ -6,18 +6,17 @@ require_once('Config.php');
 
 class Data
 {
-	private		
-		$session_id 	= NULL,
-		$session_data	= NULL,
-		$expire			= NULL,
-		$source			= NULL,
-		$ip				= NULL;
+	private	$session_id 	= NULL;
+    private $session_data	= '';     // Must be empty string. PHP 7.1+ throws error if we return NULL.
+    private $expire			= NULL;
+    private $source			= NULL;
+    private $ip				= NULL;
 		
 	// Accessors
 	public function get_session_data()
 	{
 		return $this->session_data;
-	}	
+	}
 }
 
 ?>
